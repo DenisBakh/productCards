f5cc70587179689f620bebb02ef6b8ab34307128
DenBakh <den.bah89@gmail.com>
2020-08-29 12:47:49 +0700

    Almost done, just clean the code

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,2 +1,27 @@
 //=================index.js
 //==================================product-card================================
+
+$(document).ready(function ($) {
+	// FOR QUANTITY
+	$('.card').on('click', function (e) {
+		const $this = $(this).closest('.item-product');
+
+		selectCard(e, $this);
+	});
+
+	$('.product-footer__buy').on('click', function (e) {
+		const $this = $(this).closest('.item-product');
+
+		selectCard(e, $this);
+	});
+
+	function selectCard(e, $this) {
+		e.preventDefault();
+
+		if (!$this.hasClass('disabled')) {
+			$this.toggleClass('selected');
+		}
+
+	}
+});
+
